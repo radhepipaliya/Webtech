45a5c85e020dad2e4705598cd3d102000b6473ec
~RP <radhepipaliya13112004@gmail.com>
2026-01-17 14:41:54 +0530

    Add HTML and JavaScript files for function currying example

---
+++ JavaScript/Day-09-Function5/function5.js
@@ -0,0 +1,16 @@
+//! 11. FUnciton Curring:
+//* 
+
+
+function fun1(num1){
+    console.log(num1);
+    return function fun2(num2){
+        console.log(num2+num1);
+        return function fun3(num3){
+            console.log(num3+num2+num1);
+            return "Completed";
+        };
+    };
+};
+
+console.log(fun1(10)(20)(30));
